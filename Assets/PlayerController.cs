355d22ffc05a5d9b6d3644718c9c4e9211ae6543
Óscar Muñoz <osmak@protonmail.com>
2019-03-20 21:59:29 +0100

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private int extraJumps;
+    public int extraJumpsValue; 
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public int puntuacion;
+
+    // Start is called before the first frame update
+    void Start() {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnBecameInvisible(){
+        Scene scene;
+        scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    void FixedUpdate() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight==false && moveInput > 0){
+            Flip();
+        } else if(facingRight == true && moveInput < 0){
+            Flip();
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(isGrounded==true){
+            extraJumps=extraJumpsValue;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        
+    }
+
+
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    
+}
---
+++ Assets/puntuacionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class puntuacionController : MonoBehaviour{
+
+    public int puntuacion_val;
+    Text txt_puntos;
+    // Start is called before the first frame update
+    void Start(){
+        txt_puntos = gameObject.GetComponent<Text>(); 
+        txt_puntos.text = ""+puntuacion_val;
+        InvokeRepeating("sumarPuntos", 0f, 1f);
+    }
+
+    void sumarPuntos(){
+        puntuacion_val++;
+        txt_puntos.text=""+puntuacion_val;
+    }
+
+    void Update(){
+    }
+}
